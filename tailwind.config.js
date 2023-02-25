3ecc810d262428ba1d5753c4034ec21a43483304
Rivalzy <rivalds011@gmail.com>
2023-02-25 10:47:09 +0700

    Page register

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./register.html'],
+  theme: {
+    extend: {
+      container: {
+        center : true,
+        padding: "1rem",
+      },
+      fontFamily: {
+        Poppins: ['Poppins ital']
+      }
+    },
+  },
+  plugins: [],
+}
